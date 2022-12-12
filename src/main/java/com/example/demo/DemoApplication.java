035a61b4ddcf0c3993c9c557c4e6288691677233
Andres9601 <49967902+Andres9601@users.noreply.github.com>
2022-12-12 11:28:30 -0500

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.demo.Entity","com.example.demo.Controller","com.example.demo.DTO","com.example.demo.Repository","com.example.demo.Service","com.example.demo.ServiceImpl"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
